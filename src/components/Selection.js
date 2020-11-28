75c15b7ede67ef3a022c13ac23af95294b7be274
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-28 19:24:55 +0530

    Updated Partial Code

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,20 @@
+import React, { Component } from "react";
 
+class Selection extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                background:'',
+            }
+    }
+    render () {
+        return (
+            <div className="fix-box" onClick={props.applyColor}>
+                <h2 className="subheading">Selection</h2>
+            </div>
+        );
+    }
+    
+}
+
+export default Selection;
