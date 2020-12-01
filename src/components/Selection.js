14c2b9f2a984a2977c05e42d8e68857baefdf177
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-12-01 14:14:22 +0530

    corrected code

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
 export default function Selection(props) {
-    const [tempBackground, changeBackground] = useState({ background: "" });
+    const [tempBackground, changeTempBackground] = useState({ background: "" });
     const { applyColor } = props;
 
     return (
-        <div className='fix-box' style={tempBackground} onClick={() => applyColor(changeBackground)}>
+        <div className='fix-box' style={tempBackground} onClick={() => applyColor(changeTempBackground)}>
         <h2 className='subheading'>Selection</h2>
         </div>
     );
